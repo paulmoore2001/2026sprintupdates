e7314348e25f6d50d043a50f323d516becda7f1d
paulmoore2001 <paul@startgarden.com>
2026-09-10 17:16:13 -0400

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-// ============================================================
-// After you deploy the Apps Script (see README.md, Step 2),
-// paste the Web App URL it gives you between the quotes below.
-// It looks like: https://script.google.com/macros/s/AKfycb.../exec
-// ============================================================
-const CONFIG = {
-  SCRIPT_URL: "https://script.google.com/macros/s/AKfycby7NvLbo9YoUzxReyXPmfqzZn3MxX1uGBU_P_paYCA3BsJmCKJVLk49d78jYfCQJJCXZQ/exec"
-};
