960e590e9ee7bbc3acb9c0c8f0c87f40cbde0092
paulmoore2001 <paul@startgarden.com>
2026-09-11 10:32:07 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+// ============================================================
+// After you deploy the Apps Script (see README.md, Step 2),
+// paste the Web App URL it gives you between the quotes below.
+// It looks like: https://script.google.com/macros/s/AKfycb.../exec
+// ============================================================
+const CONFIG = {
+  SCRIPT_URL: "https://script.google.com/macros/s/AKfycbzWiFx2AmtX9rkg5GDKYb1zmQyaaWCuEXPLHrL0AFC_IVnMAR2GreUxD7IDoTgRFuQyfA/exec"
+};
